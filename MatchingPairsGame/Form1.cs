2e8126a54bac0f789642bafb0f68d64ad21088a0
kronosGR <geo.elgeo@gmail.com>
2022-05-06 13:37:30 +0200

    added list of icons

--- MatchingPairsGame/Form1.cs
+++ MatchingPairsGame/Form1.cs
@@ -12,6 +12,15 @@ namespace MatchingPairsGame
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "!","!","N","N",",",",","k","k",
+            "b","b","v","v","w","w","z","z",
+
+        }
+
+
         public Form1()
         {
             InitializeComponent();
